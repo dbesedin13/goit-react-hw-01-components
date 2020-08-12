fdc38ac4f1a32b9ea04913f6821eac0d2aed7d99
dbesedin13 <belolikos@gmail.com>
2020-08-12 17:01:26 +0300

    Updates

---
+++ precache-manifest.5460cd0e5d0af63dd82c59bd2f514d37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7708c6f5b5a316d1747bdf72f260c0ef",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "548c5f0f618e4e20e8c4",
+    "url": "/goit-react-hw-01-components/static/css/main.96606f3a.chunk.css"
+  },
+  {
+    "revision": "a3351f6cfa56a07d69bd",
+    "url": "/goit-react-hw-01-components/static/js/2.3621abde.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3621abde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "548c5f0f618e4e20e8c4",
+    "url": "/goit-react-hw-01-components/static/js/main.43ca094a.chunk.js"
+  },
+  {
+    "revision": "0c4e5db3f9edfa72f8d9",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.8d811115.js"
+  }
+]);
